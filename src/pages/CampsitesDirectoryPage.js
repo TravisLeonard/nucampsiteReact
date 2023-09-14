c98b10876e557189795f0fa3d732673d86c00bb6
Travis Leonard <travis.leonard.tl@gmail.com>
2023-09-14 14:12:46 -0600

    breadcrumbs and subheader

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,11 +1,13 @@
 import { Container } from 'reactstrap';
 import CampsitesList from '../features/campsites/CampsitesList';
+import SubHeader from '../components/SubHeader';
 
 const CampsitesDirectoryPage = () => {
 
     return (
         <Container>
-                    <CampsitesList />
+            <SubHeader current='Directory' />
+            <CampsitesList />
         </Container>
     );
 };
