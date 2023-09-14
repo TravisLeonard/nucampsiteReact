75fa3f037b9b75ac858391425df6e5f1892863f3
Travis Leonard <travis.leonard.tl@gmail.com>
2023-09-14 11:18:08 -0600

    homePage images with SPA

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -8,6 +8,12 @@ export const selectCampsiteById = (id) => {
     return CAMPSITES.find((campsite) => campsite.id === id);
 };
 
+export const selectFeaturedCampsite = () => {
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
+
+
+
 //export const selectRandomCampsite = () => {
   //  return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
 
